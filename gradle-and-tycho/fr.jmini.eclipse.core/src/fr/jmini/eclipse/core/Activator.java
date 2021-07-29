ac3e330cfc429b71ef1f2c854c7fc5f0c1d85fed
Jeremie Bresson <dev@jmini.fr>
2021-07-29 09:33:36 +0200

    Add 'gradle-and-tycho' example

---
+++ gradle-and-tycho/fr.jmini.eclipse.core/src/fr/jmini/eclipse/core/Activator.java
@@ -0,0 +1,26 @@
+package fr.jmini.eclipse.core;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+	
+	public static String getText() {
+		return "Hello, Eclipse world";
+	}
+
+}
